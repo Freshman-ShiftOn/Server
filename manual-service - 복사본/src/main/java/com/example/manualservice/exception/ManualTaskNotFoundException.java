c1b3a2a227c30e7ce17aca8f159a3001df988c26
hancrysta1 <hancrystal@sookmyung.ac.kr>
2025-01-01 17:58:42 +0900

    fix: Maven to Gradle + Manual server, euraka서버 연결 + 동적 포트 할당 적용

--- manual-service - 복사본/src/main/java/com/example/manualservice/exception/ManualTaskNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.manualservice.exception;
-
-public class ManualTaskNotFoundException extends RuntimeException {
-    public ManualTaskNotFoundException(String message) {
-        super(message);
-    }
-}
