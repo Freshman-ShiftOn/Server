1d32f16dfc23aadbc43db5715922ba82dcd72019
hancrysta1 <hancrystal@sookmyung.ac.kr>
2025-01-05 04:51:05 +0900

    feat: OAuth2.0 카카오 로그인 구현(JWT인증 로직 적용)

---
+++ user-service/src/main/java/com/epicode/userservice/security/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.epicode.userservice.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final Key SECRET_KEY;
+    private static final long EXPIRATION_TIME = 86400000; // 24 hours
+
+
+    public JwtUtil(@Value("${jwt.secret-key}") String secretKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(secretKey);
+        this.SECRET_KEY = Keys.hmacShaKeyFor(decodedKey);
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    // JWT 유효성 검사
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
