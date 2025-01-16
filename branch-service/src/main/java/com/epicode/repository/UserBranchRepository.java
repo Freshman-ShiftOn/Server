fdcb20243dfdc9838406c4a2d83f73d722b6d706
SooJeong Han <hancrystal@sookmyung.ac.kr>
2025-01-16 23:33:58 +0900

    fix: repo에 쿼리 어노테이션  추가

--- branch-service/src/main/java/com/epicode/repository/UserBranchRepository.java
+++ branch-service/src/main/java/com/epicode/repository/UserBranchRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Repository
 public interface UserBranchRepository extends JpaRepository<UserBranch, Long> {
-    //@Query("SELECT ub.branch.id FROM UserBranch ub WHERE ub.user.id = :userId")
+    @Query("SELECT ub.branch.id FROM UserBranch ub WHERE ub.user.id = :userId")
     List<Long> findBranchIdsByUserId(@Param("userId") Long userId);
 //    @Query("SELECT b.name FROM Branch b WHERE b.id IN :branchIds")
 //    List<String> findBranchNamesByIds(@Param("branchIds") List<Long> branchIds);
