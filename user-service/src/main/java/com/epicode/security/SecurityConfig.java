ff3ca53becd589caeca181a03312bf6dd108b9f3
unknown <hancrystal@sookmyung.ac.kr>
2025-04-17 00:47:16 +0900

    fix: PasswordEncoder Config에 Bean등록

--- user-service/src/main/java/com/epicode/security/SecurityConfig.java
+++ user-service/src/main/java/com/epicode/security/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +14,11 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Value("${jwt.secret-key}")
     private String secretKey;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
