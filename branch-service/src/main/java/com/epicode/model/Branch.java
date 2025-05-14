4b6614b9e6d55ce38877496894fe6ed21efc052b
unknown <hancrystal@sookmyung.ac.kr>
2025-05-14 17:31:21 +0900

    feat: 직원 정보 수정 기능

--- branch-service/src/main/java/com/epicode/model/Branch.java
+++ branch-service/src/main/java/com/epicode/model/Branch.java
@@ -10,28 +10,5 @@ import java.util.List;
 @Entity
 @Table(name = "branches")
 public class Branch {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, length = 100, unique = true)
-    private String name;
-
-    @Column(nullable = false)
-    private String adress;
-
-    @Column(nullable = true)
-    private String dial_numbers;
-
-    @Column(columnDefinition = "VARCHAR(255) DEFAULT '10030'")
-    private String basic_cost = "10030";
-
-    @Column
-    private Boolean weekly_allowance;
-
-    @Column(nullable = true, length = 2083)
-    private String images;
-
-    @Column(nullable = true)
-    private String contents;
+    
 }
--- branch-service/src/main/java/com/epicode/service/UserBranchService.java
+++ branch-service/src/main/java/com/epicode/service/UserBranchService.java
@@ -6,4 +6,5 @@ public interface UserBranchService {
     void addUserAtBranch(WorkerRequestDTO dto);
     void joinBranch(Long userId, Long branchId);
     String deleteUserBranchByEmailAndBranchId(String email, Long branchId);
+    public void updateUserAtBranch(WorkerRequestDTO dto);
 }
